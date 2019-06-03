8fe390d62f543bda88a4263e788d2fcba3b5cdaf
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-06-03 23:22:59 +0200

    Adds StoreController and creates initial data in product repository

--- spring/src/main/java/com/abevilacqua/techstore/TechstoreApplication.java
+++ spring/src/main/java/com/abevilacqua/techstore/TechstoreApplication.java
@@ -1,13 +1,29 @@
 package com.abevilacqua.techstore;
 
+import com.abevilacqua.techstore.model.Product;
+import com.abevilacqua.techstore.repository.ProductRepo;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class TechstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechstoreApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner run(ProductRepo productRepo) {
+		return args -> {
+			Stream.of(
+					new Product(1, "Mouse", "Really good mouse", 20),
+					new Product(2, "Notebook", "Great notebook", 1000))
+					.forEach(productRepo::addProduct);
+		};
 	}
 
 }
--- spring/src/main/java/com/abevilacqua/techstore/controller/StoreController.java
+++ spring/src/main/java/com/abevilacqua/techstore/controller/StoreController.java
@@ -1,15 +1,38 @@
 package com.abevilacqua.techstore.controller;
 
+import com.abevilacqua.techstore.model.Product;
+import com.abevilacqua.techstore.repository.ProductRepo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class StoreController {
 
+    private ProductRepo productRepo;
+
+    public StoreController(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
     @GetMapping("/")
     public ResponseEntity helloStore() {
         return new ResponseEntity<>("Hey there from TechStore!!!", HttpStatus.OK);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        return new ResponseEntity<>(productRepo.getProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") final String id) {
+        return productRepo.getProduct(Long.valueOf(id))
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
