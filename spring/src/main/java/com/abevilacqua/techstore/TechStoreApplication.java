16d91b24c32902cd585aee78534384f617729267
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-06-19 16:53:56 +0200

    Creates new class DBInitializer in Spring project to keep the same project structure.

--- spring/src/main/java/com/abevilacqua/techstore/TechStoreApplication.java
+++ spring/src/main/java/com/abevilacqua/techstore/TechStoreApplication.java
@@ -1,13 +1,12 @@
 package com.abevilacqua.techstore;
 
-import com.abevilacqua.techstore.model.Product;
 import com.abevilacqua.techstore.repository.ProductRepo;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.stream.Stream;
+import static com.abevilacqua.techstore.configuration.DBInitializer.initializeDB;
 
 @SpringBootApplication
 public class TechStoreApplication {
@@ -18,15 +17,7 @@ public class TechStoreApplication {
 
 	@Bean
 	public ApplicationRunner run(ProductRepo productRepo) {
-		return args -> {
-			Stream.of(
-					new Product(1, "Mouse", "Really good mouse", 20),
-					new Product(2, "Notebook", "Great notebook", 1000),
-					new Product(3, "Keyboard", "Great keyboard", 300),
-					new Product(4, "Headphones", "Great headphones", 60),
-					new Product(5, "Touchscreen", "Great screen", 500))
-					.forEach(productRepo::addProduct);
-		};
+		return initializeDB(productRepo);
 	}
 
 }
