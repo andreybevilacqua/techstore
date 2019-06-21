88e6d8b15e5188fd756b8bfe8d02a85c9a08c047
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-06-21 17:04:10 +0200

    Adds getProductById() method. Adds ActivateRequestContext In AppLifecycleBean.

--- quarkus/src/main/java/com/abevilacqua/techstore/controller/StoreController.java
+++ quarkus/src/main/java/com/abevilacqua/techstore/controller/StoreController.java
@@ -2,12 +2,13 @@ package com.abevilacqua.techstore.controller;
 
 import com.abevilacqua.techstore.model.Product;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
+
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -21,5 +22,13 @@ public class StoreController {
     @Path("/products")
     public List<Product> getProducts() {
         return Product.listAll();
+    }
+
+    @GET
+    @Path("/product/{id}")
+    public Response getProductById(@PathParam("id") long id) {
+        Optional<Product> product = Optional.ofNullable(Product.findById(id));
+        if(product.isPresent()) return Response.ok(product.get()).build();
+        else return Response.status(NOT_FOUND).build();
     }
 }
