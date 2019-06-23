47069347bdea4de318acf6268eda97df2a4f334c
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-06-23 21:25:12 +0200

    Adds unit tests

--- quarkus/src/test/java/com/abevilacqua/techstore/TechStoreTest.java
+++ quarkus/src/test/java/com/abevilacqua/techstore/TechStoreTest.java
@@ -4,6 +4,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.ws.rs.core.MediaType;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
@@ -30,6 +32,33 @@ class TechStoreTest {
                 .then()
                 .statusCode(200)
                 .body(containsString("Mouse"), containsString("Really good mouse"));
+    }
+
+    @Test
+    @DisplayName("Should create product")
+    void shouldCreateProduct() {
+        given()
+                .body("{\"name\": \"New Product\", \"description\": \"Really good new product\", \"price\": \"256\"}")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(201)
+                .body("name", containsString("New Product"),
+                        "description", containsString("Really good new product"),
+                        "price", containsString("256"));
+    }
+
+    @Test
+    @DisplayName("Should update product")
+    void shouldUpdateProduct() {
+
+    }
+
+    @Test
+    @DisplayName("Should delete product")
+    void shouldDeleteProduct() {
+
     }
 
 }
