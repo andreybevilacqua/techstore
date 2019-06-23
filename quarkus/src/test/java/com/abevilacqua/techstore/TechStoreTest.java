5ea78f617e147b59b800c008848ec67aa30ec39b
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-06-23 20:27:19 +0200

    Adds unit tests to StoreController.

--- quarkus/src/test/java/com/abevilacqua/techstore/TechStoreTest.java
+++ quarkus/src/test/java/com/abevilacqua/techstore/TechStoreTest.java
@@ -1,19 +1,35 @@
 package com.abevilacqua.techstore;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class TechStoreTest {
 
     @Test
-    void testHelloEndpoint() {
+    @DisplayName("Should get list of Products")
+    void shouldGetListOfProducts() {
         given()
-          .when().get("/products/1")
-          .then()
-             .statusCode(200);
+                .when()
+                .get("/products")
+                .then()
+                .body("$.size()", is(5));
+    }
+
+    @Test
+    @DisplayName("Should find product by ID")
+    void shouldFindProductByID() {
+        given()
+                .when()
+                .get("/products/1")
+                .then()
+                .statusCode(200)
+                .body(containsString("Mouse"), containsString("Really good mouse"));
     }
 
 }
