9f93afec6b854fc146d2b046a6231fb24822263f
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-08-01 10:49:08 +0200

    Changes PostgreSQL connection URL in application.properties. To avoid warning, don't call super in equals and hashcode.

--- quarkus/src/main/java/com/abevilacqua/techstore/model/Product.java
+++ quarkus/src/main/java/com/abevilacqua/techstore/model/Product.java
@@ -3,6 +3,7 @@ package com.abevilacqua.techstore.model;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import javax.persistence.Entity;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class Product extends PanacheEntity {
 
     // PanacheEntity already have a Long ID implemented.
